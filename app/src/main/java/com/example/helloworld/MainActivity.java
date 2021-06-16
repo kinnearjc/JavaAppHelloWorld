4a205e492e96a2964a9a8517ef8b3d9329d7bdce
Joey K <kinnearjc@gmail.com>
2021-06-16 19:14:28 -0400

    minor changes to improve class (laptop)

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String tag ="Main Activity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(tag, "Logging like a pro!");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
